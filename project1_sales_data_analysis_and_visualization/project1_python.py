a0210dc29de6796bbd92977b1e3aa79bebfee2a5
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-07-21 13:54:46 +0530

    Introduced modulity in the code along with best practices like Data cleaning and Descriptibe Docstrings for each function

--- project1_sales_data_analysis_and_visualization/project1_python.py
+++ project1_sales_data_analysis_and_visualization/project1_python.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 """
 Manager has given task to...
@@ -10,32 +11,67 @@ Manager has given task to...
 - Visualize sales trends over time
 - Find the best-performing month
 """
+#Create a funciton to load and clean the data
+def load_and_clean_data(filepath:str)->pd.DataFrame:
+    """Load and clean csv file from the given file path and return dataframe built from csv"""
 
-#Load the CSV file
-sales_data = pd.read_csv("sales_data.csv")
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"File '{filepath}' not found.")
+    
+    df = pd.read_csv(filepath)
+    #clean the column names
+    df.columns = df.columns.str.strip().str.capitalize()    #Remove whitespaces from index names and capitalize the first character of each word
+    df["Month"] = df["Month"].astype(str).str.strip()
+    
+    df=df.dropna(subset=["Sales","Month"])
+    #This will drop rows where either sales or Month any of them is empty
+    df["Sales"] = pd.to_numeric(df["Sales"], errors="coerce")
+    #Convert each cell in Sales column to numeric, and if can't throw an error
+    df=df.dropna(subset=["Sales"])
+    #drop rows where sales couldn't be converted
+    return df
 
-#convert sales column to numpy for math operations
-only_sales = np.array(sales_data.Sales)
+#function to calculate total and average from sales column
+def calc_total_and_average(df:pd.DataFrame)->tuple[float,float]:
+    """calculate total and average in the dataframe and return float"""
 
-#calculating total and averages
-total_sales = np.sum(only_sales)
-average_sales = np.mean(only_sales)
+    total = df["Sales"].sum()
+    average = df["Sales"].mean()
+    return total, round(average, 3)
 
-print(f"Tatal sales: {total_sales}")
-print(f"Average sales each month: {round(average_sales, 3)}")
+#function to find the best performing month
+def best_month(df:pd.DataFrame)->str:
+    """find the best month based on sales and return month name as string"""
+
+    return df.loc[df["Sales"].idxmax(), "Month"]
+    #df["Sales"].idxmax returns the NAME of the index as loc operates with names of index and columns
+
+    #return df.iloc[df["Sales"].argmax(), df.columns.get_loc("Month")]
+    #df["Sales"].argmax() returns the index of max number
+    #df.columns.get_loc("Month") returns the index of column labeled "Month"
+    #Using both numerical values with iloc to get the accurate cell and returning it which would be string
+
+def plot_chart(df:pd.DataFrame):
+    """plot the line chart for the given dataframe"""
+
+    #using matplotlib to visualize the sales
+    plt.plot(df["Month"], df["Sales"], marker="o", color="red")
+    plt.title("Monthly Sales Comparison")
+    plt.xlabel("Month")
+    plt.ylabel("Sales")
+    plt.grid(True)
+    plt.show()
 
 
-#using matplotlib to visualize the sales
-plt.plot(sales_data["Month"], sales_data["Sales"])
-plt.bar(sales_data["Month"], sales_data["Sales"])
-plt.title("Monthly sales comparision")
-plt.xlabel("Month")
-plt.ylabel("Sales")
-plt.show()
+#Main functions of the class
+sales_data = load_and_clean_data("sales_data.csv")
+total_sales, average_sales = calc_total_and_average(sales_data)
+top_month = best_month(sales_data)
+
+print(f"Total sales: {total_sales}")
+print(f"Average sales each month: {average_sales}")
+print(f"Best performing Month: {top_month}")
+
+plot_chart(sales_data)
 
 
-#Find the best performing month
-#get the index first and than use iloc on actual data to get the row
-max_sales_index = np.argmax(only_sales)
-best_month = sales_data.iloc[max_sales_index, sales_data.columns.get_loc("Month")]
-print(f"Best performing Month: {best_month}")
