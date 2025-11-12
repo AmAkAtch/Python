f1707ab3bdecc74979f7ead7998280f1c4ea082c
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-11-12 13:27:24 +0530

    Drawing Shapes using colors

--- 100_days_of_code/day18_turtle/basic_practice/turtle_basics.py
+++ 100_days_of_code/day18_turtle/basic_practice/turtle_basics.py
@@ -1,14 +1,23 @@
 from turtle import Turtle, Screen
+import random
 
 turtle = Turtle()
+turtle_colors = [
+    "white", "black", "red", "green", "blue", "yellow", "cyan", "magenta",
+    "gray", "orange", "purple", "brown", "pink", "gold", "silver", "navy",
+    "maroon", "olive", "teal", "lime", "skyblue", "turquoise", "salmon",
+    "coral", "orchid", "plum", "indigo", "violet", "tan", "beige", "chocolate",
+    "crimson", "darkgreen", "darkblue", "darkred", "lightblue", "lightgreen",
+    "lightgray", "lavender", "thistle", "tomato", "wheat", "seashell", "snow"
+]
+#Each shape has same sides and same angles
 
-for turn in range (4):
-    turtle.forward(100)
-    turtle.right(90)
-
-
-
-
+for sides in range (3, 11):
+    turtle.color(random.choice(turtle_colors))
+    for side in range(sides):
+        turtle.forward(100)
+        angle = (sides-2)*180/sides
+        turtle.right(180-angle)
 
 
 screen = Screen()
