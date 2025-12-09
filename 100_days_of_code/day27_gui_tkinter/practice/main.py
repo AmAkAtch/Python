3e623cb8e532086aa3dff3331e29e46fabf28300
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-12-09 16:18:11 +0530

    Simple Text input program

--- 100_days_of_code/day27_gui_tkinter/practice/main.py
+++ 100_days_of_code/day27_gui_tkinter/practice/main.py
@@ -1,11 +1,21 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI window")
 window.minsize(width=500, height=300)
 
-label = tkinter.Label(text="Hello from windows", font=("Cambria",15, "bold"))
-label.pack(side="bottom", )
+
+def on_click():
+    label.config(text=user_input.get())
+
+label = Label(text="Hello from windows", font=("Cambria",15, "bold"))
+label.pack()
+
+button = Button(text="click me", command=on_click)
+button.pack()
+
+user_input = Entry()
+user_input.pack()
 
 
 window.mainloop()
