4be0e80e3b5e5febfb8cfeb722f626795611f318
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-12-17 16:28:50 +0530

    Impletedmented function to generate_password - Added functionality to directly copy new generated password to clipboar with the help of pyperclip

--- 100_days_of_code/day29_gui_password_manager/main.py
+++ 100_days_of_code/day29_gui_password_manager/main.py
@@ -1,22 +1,50 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 #Constants
 DEFAULT_EMAIL = "gadhavirushiraj7@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    print("Generate Password")
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+    
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+
+    input_password.insert(0, password)
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    if input_website.get() and input_email.get() and input_password.get():
-        with open("day29_gui_password_manager/passwords.txt", "a") as file:
-            file.write(f'\n{input_website.get()} | {input_email.get()} | {input_password.get()}')
-            input_website.delete(0, END)
-            input_password.delete(0, END)
-            input_website.focus()
-            print("Password Saved")
+    website = input_website.get()
+    email = input_email.get()
+    password = input_password.get()
+
+    if website and email and password:
+        is_ok = messagebox.askokcancel(title=website, message=f"Email: {email} \nPassword: {password} \n is this correct?")
+        if is_ok:
+            with open("day29_gui_password_manager/passwords.txt", "a") as file:
+                file.write(f'{input_website.get()} | {input_email.get()} | {input_password.get()}\n')
+                input_website.delete(0, END)
+                input_password.delete(0, END)
+                input_website.focus()
+                messagebox.showinfo(title="Password Saved", message="Details Saved Successfully")
+                print("Password Saved")
     else:
         messagebox.showwarning(title="Missing Data", message="Please fill all fields.")
 # ---------------------------- UI SETUP ------------------------------- #
