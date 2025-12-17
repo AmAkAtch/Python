1fcdbb90b25d0893328bee9b9b7a00d93978cc17
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-12-17 13:22:13 +0530

    Initial commit for Day 29 password manager

---
+++ 100_days_of_code/day29_gui_password_manager/main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.minsize(width=400, height=600)
+window.title("Password Manager")
+
+cv = Canvas()
+logo = PhotoImage(file="day29_gui_password_manager/logo.png")
+# cv.create_image(100, 150, image_names(logo))
+
+
+
+
+
+
+
+window.mainloop()
