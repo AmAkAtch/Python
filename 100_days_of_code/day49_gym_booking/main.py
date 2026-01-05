5326b8ef2ba913497517f4c85915924fd23493e6
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2026-01-05 16:56:35 +0530

    Completed day 49

--- 100_days_of_code/day49_gym_booking/main.py
+++ 100_days_of_code/day49_gym_booking/main.py
@@ -2,11 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
+today = datetime.now()
+day_today = today.strftime("%A")
+print(day_today)
 
 #CONSTANTs
 USERNAME = "trial"
 PASSWORD = "trial84"
 EMAIL = "trialid84@gmail.com"
+
+#information
+classes_booked=0
+waitlists_joined=0
+already_booked_waitlisted=0
+total_tuesday_6pm_classes_processed=0
+total_thursday_6pm_classes_processed=0
 
 options=Options()
 options.add_experimental_option("detach", True)
@@ -14,6 +28,8 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://appbrewery.github.io/gym/")
+
+days_tobe_booked = ['tue', 'thu']
 
 def login_sequence():
     try:
@@ -46,5 +62,31 @@ def login_sequence():
         print("Trying Again...")
         login_sequence()
 
+def book_tuesday_class(day):
+    global classes_booked, already_booked_waitlisted, waitlists_joined, total_tuesday_6pm_classes_processed, total_thursday_6pm_classes_processed
+    print(f"Starting Booking process for {day}")
+    tuesday_classes = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR,f"div[id*='{day}']"))
+    )    
+    six_pm_class = tuesday_classes.find_element(By.CSS_SELECTOR, value="div[id*='-1800']")
+    book_button = six_pm_class.find_element(By.TAG_NAME, value="button")
+    if six_pm_class.get_attribute("data-user-booked")!="true":
+        if book_button.text == "Join Waitlist":
+            waitlists_joined+=1
+            print(f"{day} waitlist Joined")
+        else:
+            classes_booked+=1
+            print(f"{day} Class Joined")
+        book_button.click()
+    else:
+        already_booked_waitlisted+=1
+        print(f"{day} Class was already Booked/waitlisted")
+    if day=="tue":
+        total_tuesday_6pm_classes_processed+=1
+    else:
+        total_thursday_6pm_classes_processed+=1
 
 login_sequence()
+for day in days_tobe_booked:
+    book_tuesday_class(day)
+print(f"classes_booked: {classes_booked}\nwaitlists_joined: {waitlists_joined}\nalready_booked_waitlisted: {already_booked_waitlisted}\ntotal_tuesday_6pm_classes_processed: {total_tuesday_6pm_classes_processed}\ntotal_thursday_6pm_classes_processed: {total_thursday_6pm_classes_processed}")
