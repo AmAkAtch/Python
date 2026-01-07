07ac6005368adb58447d882671179e01cc49ab81
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2026-01-07 10:13:20 +0530

    Added Instagram login functionality to bot

--- 100_days_of_code/day52_instagram_follower/insta_follow_bot.py
+++ 100_days_of_code/day52_instagram_follower/insta_follow_bot.py
@@ -1,8 +1,31 @@
-
-
+import os
+from dotenv import load_dotenv
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 class InstaFollowBot:
+    #load environment variables
+    load_dotenv()
+
+    #get userdata from env for instagram
+    USER_NAME = os.environ["INSTA_USER"]
+    PASSWORD = os.environ["PASSWORD"]
+
     def __init__(self, driver):
         self.driver = driver
     
-    def insta_login():
-        pass
+    def insta_login(self):
+        self.driver.get("https://instagram.com")
+        
+        username_input = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#loginForm > div.html-div.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div:nth-child(1) > div > label > input")))
+        username_input.send_keys(self.USER_NAME)
+
+        password_input = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#loginForm > div.html-div.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div:nth-child(2) > div > label > input")))
+        password_input.send_keys(self.PASSWORD, Keys.ENTER)
+
+        try:
+            save_password_element = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, '//div[@role="button" and contains(text(), "Not now")]')))
+            save_password_element.click()
+        except Exception as e:
+            print(f"Save password dialogue did not appear: {e}")
--- 100_days_of_code/day52_instagram_follower/main.py
+++ 100_days_of_code/day52_instagram_follower/main.py
@@ -1,23 +1,23 @@
-import os
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from insta_follow_bot import InstaFollowBot
 
-#load environment variables
-load_dotenv()
-
-#get userdata from env for instagram
-USER_NAME = os.environ["INSTA_USER"]
-PASSWORD = os.environ["PASSWORD"]
 
 #initialize the selenium webdriver
 options = Options()
+options.add_argument("--disable-features=PasswordLeakDetection")
 options.add_experimental_option("detach", True)
+# Option: Disable the "Save Password" bubble as well (often helpful)
+options.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False
+})
+
 driver = webdriver.Chrome(options=options)
 
 #create object for insta_follow_bot
 follow_bot = InstaFollowBot(driver)
+follow_bot.insta_login()
 
 
 
